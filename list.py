a3b4a91b67e3e8c9582ce3ecbb3573f3b71a84bb
Danone159 <d.dropco@gmail.com>
2025-04-25 09:34:16 +0200

    gde

---
+++ list.py
@@ -0,0 +1,13 @@
+letters = ["a", "b", "c","d","e","f"]
+cisla = [0,1,2,3,4,5,6,7,8,9]
+zeros = ["!","@","_","-","/"]
+matrix =[]
+
+print("pismena: ", letters)
+print("Cisla: ",cisla)
+print("Znaky: ",zeros)
+print("pismena[2] ", letters)
+print("cisla[4] ", cisla)
+print("zeros[3]: ", zeros)
+print("cisla[4]", cisla[4:])
+print("cisla[4]", cisla[:4])
--- skúška na test.py
+++
@@ -1,4 +0,0 @@
-import random
-
-random.randrange(10,23)
-print(random.randrange)
