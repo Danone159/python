7de282784a729755cce217bc4dda077f6fa1d561
Danone159 <d.dropco@gmail.com>
2025-04-25 10:02:58 +0200

    cav

--- list.py
+++ list.py
@@ -2,6 +2,11 @@ letters = ["a", "b", "c","d","e","f"]
 cisla = [0,1,2,3,4,5,6,7,8,9]
 zeros = ["!","@","_","-","/"]
 matrix =[[1,2,3],[4,5,6],[7,8,9]]
+pismena = range(30)
+
+second, *other = cisla
+print (second)
+print (other)
 
 print("pismena: ", letters)
 print("Cisla: ",cisla)
