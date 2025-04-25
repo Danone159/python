09e766b5ec48bb625c40f1a62977c06c45356ca7
Danone159 <d.dropco@gmail.com>
2025-04-25 10:15:50 +0200

    vsbjl

---
+++ strings.py
@@ -0,0 +1,8 @@
+moje_meno = "Volam sa Daniel"
+pocet_znakov = len(moje_meno)
+print("pocet znakov v mojom mene je: ", pocet_znakov)
+print("Prvy znak je ", moje_meno[0])
+print("Prvy znak je ", moje_meno[-1])
+print("Prvy znak je ", moje_meno[0:1])
+print("Prvy znak je ", moje_meno[::-1])
+print("Prvy znak je ", moje_meno[-3:])
